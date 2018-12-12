939477c3c059cd63d98e1abf626da892a3aee175
znzoux <znzoux@SX-BG-047>
2018-12-12 20:31:59 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/ZoomMode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    /// <summary>
+    /// Specifies the zoom mode for the <see cref="CoolPrintPreviewControl"/> control.
+    /// </summary>
+    public enum ZoomMode
+    {
+        /// <summary>
+        /// Show the preview in actual size.
+        /// </summary>
+        ActualSize,
+        /// <summary>
+        /// Show a full page.
+        /// </summary>
+        FullPage,
+        /// <summary>
+        /// Show a full page width.
+        /// </summary>
+        PageWidth,
+        /// <summary>
+        /// Show two full pages.
+        /// </summary>
+        TwoPages,
+        /// <summary>
+        /// Use the zoom factor specified by the <see cref="CoolPrintPreviewControl.Zoom"/> property.
+        /// </summary>
+        Custom
+    }
+}
